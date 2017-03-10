6e2c6370ddfe063c7cb095f28037e98f89ea7fef
sherluck08 <shefman8@gmail.com>
2017-03-10 17:56:36 +0100

    my first commit, my scripts is working nicely

---
+++ movie_trivia.py
@@ -0,0 +1,68 @@
+#6:32
+
+from bs4 import BeautifulSoup
+import requests
+
+
+
+def menu():
+    print("\n")
+    print("---------------------------------")
+    print("----------Series fun fact----------")
+    print("---------------------------------")
+    print("\n")
+
+def crawl():
+
+    running = True
+    while running:
+
+        menu()
+        print('Enter the name of the movie (Enter to quit): ')
+        search_movie = input('>>> ')
+        if search_movie != '':
+            try:
+                r = requests.get('http://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=all'.format(search_movie))
+                html = r.content
+                soup = BeautifulSoup(html, 'html.parser')
+                goto_movie_page = soup.find('td', {'class':'result_text'}).a
+                href = goto_movie_page.get('href')
+                movie_page_link = 'http://www.imdb.com' + href
+                sub_moviepage_url = movie_page_link[:-17]
+                print('The link to the search movie {} is {}'.format(search_movie, movie_page_link))
+                find_trivia_link(sub_moviepage_url, movie_page_link)
+
+            except requests.exceptions.ConnectionError:
+                print("[+]Error: Network is down..")
+
+        else:
+            print('[+] Quiting...')
+            running = False
+
+
+#now program has now reach the movie page now is time to get the trivia link
+def find_trivia_link(movie_sub_url, trivia_link):
+
+    req = requests.get(trivia_link)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    go_to_trivia_page = soup.find('a', {'class':'nobr'})
+    href = go_to_trivia_page.get('href')
+    full_trivia_page_url = movie_sub_url + href
+    trivia(full_trivia_page_url)
+
+#This function get the trivia text on the trivia page
+def trivia(url):
+
+    r = requests.get(url)
+    html = r.content
+    soup = BeautifulSoup(html, 'html.parser')
+    trivia_text = soup.find_all('div', {'class':'sodatext'})
+    for text in trivia_text:
+        print(text.get_text())
+    print('[+] This is the end of the trivia')
+
+
+
+
+if __name__ == '__main__':
+    crawl()
